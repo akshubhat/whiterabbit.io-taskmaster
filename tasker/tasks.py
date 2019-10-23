d6c7166c728f307a5dd5f0a2edaf0702497547b4
Akshay Suresh Bhat <akshaybhat2012@gmail.com>
2019-10-24 01:54:26 +0530

    Celery beat schedule and tasks added.

--- tasker/tasks.py
+++ tasker/tasks.py
@@ -1,5 +1,16 @@
 from celery import shared_task
+from celery.decorators import task
+from .models import ClientTask
 
 @shared_task
 def action_client(action="Call", client="Akshay"):
     return (action+" "+client)
+
+@task(name="complete_client_task")
+def complete_client_task():
+    tasks = ClientTask.objects.all()
+    for task in tasks:
+        if task.complete_action:
+            print("Task completed.")
+        else:
+            print("Task remaining.")
--- taskmaster/celery.py
+++ taskmaster/celery.py
@@ -1,8 +1,21 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmaster.settings')
 
 celery_app = Celery('taskmaster')
 celery_app.config_from_object('django.conf:settings')
 celery_app.autodiscover_tasks('CELERY')
+
+@celery_app.task
+def test(arg):
+    print(arg)
+
+celery_app.conf.beat_schedule = {
+    'complete-every-30-seconds': {
+        'task': 'tasks.add',
+        'schedule': 30.0,
+    },
+}
+celery_app.conf.timezone = 'UTC'
